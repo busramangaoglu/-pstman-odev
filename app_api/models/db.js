93fc3b1cc966d50913078c2a6ff3877702c7204a
Büşra Mangaoğlu <mangabusraa@gmail.com>
2025-12-14 22:58:43 +0300

    MongoDB connection via env variable

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,14 +1,27 @@
 require("dotenv").config();
+
 const mongoose = require("mongoose");
 
-const dbURI = process.env.MONGODB_URI;
+const dbURI = process.env.MONGODB_URI || "mongodb://localhost/mekanbul";
 
-mongoose.connect(dbURI)
-  .then(() => {
-    console.log("MongoDB Atlas bağlantısı başarılı:", dbURI);
-  })
-  .catch((err) => {
-    console.log("MongoDB bağlantı hatası:", err);
-  });
+mongoose.connect(dbURI);
+
+mongoose.connection.on("connected", function () {
+  console.log("MongoDB bağlantısı başarılı:", dbURI);
+});
+mongoose.connection.on("error", function (err) {
+  console.log("MongoDB bağlantı hatası:", err);
+});
+mongoose.connection.on("disconnected", function () {
+  console.log("MongoDB bağlantısı kesildi.");
+});
+
+process.on("SIGINT", function () {
+  mongoose.connection.close();
+  console.log("MongoDB uygulama sonlandırma nedeniyle bağlantıyı kapattı.");
+  process.exit(0);
+});
 
 require("./venue");
+
+
